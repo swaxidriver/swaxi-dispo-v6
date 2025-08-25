a7efce1168a583d845b51ca9094b9906511d9d4e
swaxidriver <183952339+swaxidriver@users.noreply.github.com>
2025-08-25 17:11:02 +0200

    Convert configuration files to CommonJS syntax

---
+++ vite.config.cjs
@@ -0,0 +1,8 @@
+const { defineConfig } = require('vite')
+const react = require('@vitejs/plugin-react')
+
+// https://vite.dev/config/
+module.exports = defineConfig({
+  plugins: [react()],
+  base: '/swaxi-dispo-v6/',
+})
